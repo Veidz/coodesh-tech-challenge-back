0843ab48ef96b9303759583e43533a37027818b1
Barthmosz <joaovictorveidz@gmail.com>
2023-05-21 16:03:01 -0300

    feat: implement GetAsync StaticPersistence

---
+++ CoodeshTechChallenge/CoodeshTechChallenge.Persistence/Contracts/IStaticPersistence.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoodeshTechChallenge.Persistence.Contracts
+{
+    public interface IStaticPersistence<D> where D : class
+    {
+        Task<List<D>> GetAsync();
+    }
+}
---
+++ CoodeshTechChallenge/CoodeshTechChallenge.Persistence/Persistence/StaticPersistence.cs
@@ -0,0 +1,25 @@
+﻿using CoodeshTechChallenge.Persistence.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoodeshTechChallenge.Persistence.Persistence
+{
+    public class StaticPersistence<D> : IStaticPersistence<D> where D : class
+    {
+        private readonly DbContext dbContext;
+        private readonly DbSet<D> dbSet;
+
+        public StaticPersistence(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.dbSet = this.dbContext != null ? this.dbContext.Set<D>() : throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<List<D>> GetAsync()
+        {
+            return await this.dbSet.ToListAsync();
+        }
+    }
+}
