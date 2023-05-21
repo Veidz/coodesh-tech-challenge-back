182704218db209da818fccecd6270e98317b8f80
Barthmosz <joaovictorveidz@gmail.com>
2023-05-21 19:05:09 -0300

    feat: create TransactionOutput DTO

---
+++ CoodeshTechChallenge/CoodeshTechChallenge.Application/DTO/TransactionOutput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CoodeshTechChallenge.Application.DTO
+{
+    public class TransactionOutput
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public string Product { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Seller { get; set; } = string.Empty;
+    }
+}
