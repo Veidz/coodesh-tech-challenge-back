41b5c107f6422e06287f053fbaf4cbcd2d384bbc
Barthmosz <joaovictorveidz@gmail.com>
2023-05-21 02:51:28 -0300

    chore: remove initial api files

--- CoodeshTechChallenge/CoodeshTechChallenge.API/Program.cs
+++ CoodeshTechChallenge/CoodeshTechChallenge.API/Program.cs
@@ -1,22 +1,22 @@
 
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace CoodeshTechChallenge.API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+            WebApplication app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -24,10 +24,7 @@ namespace CoodeshTechChallenge.API
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
