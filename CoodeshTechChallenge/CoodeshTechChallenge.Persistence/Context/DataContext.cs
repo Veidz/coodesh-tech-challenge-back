c047e62802adbdfbd86460e066d2ad519e70e33e
Barthmosz <joaovictorveidz@gmail.com>
2023-05-21 16:23:21 -0300

    feat: add domains DbSet to DataContext

--- CoodeshTechChallenge/CoodeshTechChallenge.Persistence/Context/DataContext.cs
+++ CoodeshTechChallenge/CoodeshTechChallenge.Persistence/Context/DataContext.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CoodeshTechChallenge.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoodeshTechChallenge.Persistence.Context
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Type> Type { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Seller> Seller { get; set; }
     }
 }
