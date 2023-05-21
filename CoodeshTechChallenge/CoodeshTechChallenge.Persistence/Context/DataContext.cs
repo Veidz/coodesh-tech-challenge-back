c714f87739206d2e57c49ec2bd275fe5e18c54ca
Barthmosz <joaovictorveidz@gmail.com>
2023-05-21 20:19:37 -0300

    chore: create base migrate and seed to db

--- CoodeshTechChallenge/CoodeshTechChallenge.Persistence/Context/DataContext.cs
+++ CoodeshTechChallenge/CoodeshTechChallenge.Persistence/Context/DataContext.cs
@@ -6,11 +6,38 @@ namespace CoodeshTechChallenge.Persistence.Context
 {
     public class DataContext : DbContext, IDataContext
     {
+        public DataContext() { }
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Type> Type { get; set; }
         public DbSet<Product> Product { get; set; }
         public DbSet<Seller> Seller { get; set; }
         public DbSet<Transaction> Transaction { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Type>().HasData(
+                new Type() { Id = 1, Description = "Venda produtor" },
+                new Type() { Id = 2, Description = "Venda afiliado" },
+                new Type() { Id = 3, Description = "Comissão paga" },
+                new Type() { Id = 4, Description = "Comissão recebida" }
+            );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product() { Id = 1, Name = "CURSO DE BEM-ESTAR" },
+                new Product() { Id = 2, Name = "DOMINANDO INVESTIMENTOS" },
+                new Product() { Id = 3, Name = "DESENVOLVEDOR FULL STACK" }
+            );
+
+            modelBuilder.Entity<Seller>().HasData(
+                new Seller() { Id = 1, Name = "JOSE CARLOS" },
+                new Seller() { Id = 2, Name = "MARIA CANDIDA" },
+                new Seller() { Id = 3, Name = "THIAGO OLIVEIRA" },
+                new Seller() { Id = 4, Name = "ELIANA NOGUEIRA" },
+                new Seller() { Id = 5, Name = "CARLOS BATISTA" },
+                new Seller() { Id = 6, Name = "CAROLINA MACHADO" },
+                new Seller() { Id = 7, Name = "CELSO DE MELO" }
+            );
+        }
     }
 }
---
+++ CoodeshTechChallenge/CoodeshTechChallenge.Persistence/Migrations/20230521231840_InitialCreate.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CoodeshTechChallenge.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sellers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sellers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Product = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Seller = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Types",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Types", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "CURSO DE BEM-ESTAR" },
+                    { 2, "DOMINANDO INVESTIMENTOS" },
+                    { 3, "DESENVOLVEDOR FULL STACK" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Sellers",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "JOSE CARLOS" },
+                    { 2, "MARIA CANDIDA" },
+                    { 3, "THIAGO OLIVEIRA" },
+                    { 4, "ELIANA NOGUEIRA" },
+                    { 5, "CARLOS BATISTA" },
+                    { 6, "CAROLINA MACHADO" },
+                    { 7, "CELSO DE MELO" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Types",
+                columns: new[] { "Id", "Description" },
+                values: new object[,]
+                {
+                    { 1, "Venda produtor" },
+                    { 2, "Venda afiliado" },
+                    { 3, "Comissão paga" },
+                    { 4, "Comissão recebida" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Sellers");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Types");
+        }
+    }
+}
