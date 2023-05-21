2623d6153f9a7b5a6f5eafb2037e8b4de2dc4057
Barthmosz <joaovictorveidz@gmail.com>
2023-05-21 16:03:17 -0300

    feat: implement PostListAsync DynamicPersistence

---
+++ CoodeshTechChallenge/CoodeshTechChallenge.Persistence/Contracts/IDynamicPersistence.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoodeshTechChallenge.Persistence.Contracts
+{
+    public interface IDynamicPersistence<D> where D : class
+    {
+        Task<List<D>> PostListAsync(List<D> domainList);
+    }
+}
---
+++ CoodeshTechChallenge/CoodeshTechChallenge.Persistence/Persistence/DynamicPersistence.cs
@@ -0,0 +1,38 @@
+﻿using CoodeshTechChallenge.Persistence.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoodeshTechChallenge.Persistence.Persistence
+{
+    public class DynamicPersistence<D> : IDynamicPersistence<D> where D : class
+    {
+        private readonly DbContext dbContext;
+        private readonly DbSet<D> dbSet;
+
+        public DynamicPersistence(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.dbSet = this.dbContext != null ? this.dbContext.Set<D>() : throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<List<D>> PostListAsync(List<D> domainList)
+        {
+            if (domainList == null)
+            {
+                throw new ArgumentNullException(nameof(domainList));
+            }
+            else
+            {
+                foreach (D domain in domainList)
+                {
+                    this.dbSet.Add(domain);
+                }
+
+                await this.dbContext.SaveChangesAsync();
+                return domainList;
+            }
+        }
+    }
+}
