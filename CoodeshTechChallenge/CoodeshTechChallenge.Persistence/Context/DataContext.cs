8a7cd1168e9a1e92e8e8151c061ca8df8d818c79
Barthmosz <joaovictorveidz@gmail.com>
2023-05-21 16:01:53 -0300

    feat: implement base DataContext

---
+++ CoodeshTechChallenge/CoodeshTechChallenge.Persistence/Context/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoodeshTechChallenge.Persistence.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
